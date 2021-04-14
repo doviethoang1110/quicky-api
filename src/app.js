979e1f168769fa4d00e2e65a10760e6d6763ff46
Viethoang <hviet371@gmail.com>
2021-04-14 09:02:37 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({version: '0.1', projectName: 'Quicky-messenge-apir'});
+});
+
 app.get('/hello', (req, res) => {
   res.send("ok  hello 123")
 });
